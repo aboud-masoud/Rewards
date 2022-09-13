051a9f62f757d58883871c070f4c041d0f4ba2ac
Equiti <equiti@Equitis-MacBook-Pro.local>
2022-09-13 11:10:20 +0300

    add spash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-// import 'package:hive_flutter/hive_flutter.dart';
 import 'package:rewards_app/app.dart';
 import 'package:rewards_app/utils/logger.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  // await Hive.initFlutter();
   logDebugMessage(message: "Application Started ... ");
 
   SystemChrome.setPreferredOrientations([
