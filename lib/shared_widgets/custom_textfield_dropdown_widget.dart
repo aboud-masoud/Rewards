21ff0f4e63d89bed927fddf0a6850c8aa4e0ea63
Abdelrahman <abdelrahman@JOD0242L.local>
2022-09-21 08:42:51 +0300

    .

--- lib/shared_widgets/custom_textfield_dropdown_widget.dart
+++ lib/shared_widgets/custom_textfield_dropdown_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rewards_app/utils/custom_text.dart';
 import 'package:rewards_app/utils/custom_text_style.dart';
 
 class CustomTextFieldWithDropDown extends StatelessWidget {
@@ -21,35 +22,47 @@ class CustomTextFieldWithDropDown extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 16, right: 16),
-      child: FormField(builder: (FormFieldState<String> state) {
-        return InputDecorator(
-          decoration: InputDecoration(
-            prefixIcon: prefixIcon,
-            labelStyle: CustomTextStyle().regular(size: 16.0),
-            errorStyle:
-                CustomTextStyle().regular(color: Colors.redAccent, size: 16.0),
-            hintText: hintMessage,
-          ),
-          isEmpty: value == '',
-          child: DropdownButtonHideUnderline(
-            child: DropdownButton<String>(
-              value: value == "" ? null : value,
-              isDense: true,
-              onChanged: (String? newValue) {
-                if (newValue != null) {
-                  // value = newValue;
-                  onChanged!(newValue);
-                  state.didChange(newValue);
-                }
-              },
-              items: items
-                  .map((String item) =>
-                      DropdownMenuItem<String>(child: Text(item), value: item))
-                  .toList(),
-            ),
-          ),
-        );
-      }),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          value != ''
+              ? CustomText(
+                  title: hintMessage,
+                  style: CustomTextStyle()
+                      .regular(size: 12, color: const Color(0xff707070)),
+                )
+              : Container(),
+          FormField(builder: (FormFieldState<String> state) {
+            return InputDecorator(
+              decoration: InputDecoration(
+                prefixIcon: prefixIcon,
+                labelStyle: CustomTextStyle().regular(size: 16.0),
+                errorStyle: CustomTextStyle()
+                    .regular(color: Colors.redAccent, size: 16.0),
+                hintText: hintMessage,
+              ),
+              isEmpty: value == '',
+              child: DropdownButtonHideUnderline(
+                child: DropdownButton<String>(
+                  value: value == "" ? null : value,
+                  isDense: true,
+                  onChanged: (String? newValue) {
+                    if (newValue != null) {
+                      // value = newValue;
+                      onChanged!(newValue);
+                      state.didChange(newValue);
+                    }
+                  },
+                  items: items
+                      .map((String item) => DropdownMenuItem<String>(
+                          child: Text(item), value: item))
+                      .toList(),
+                ),
+              ),
+            );
+          }),
+        ],
+      ),
     );
   }
 
